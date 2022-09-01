c864f95a2dba5ab0e132b0e9123a88c11b81435a
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2022-09-02 00:20:10 +0200

    Added User model to admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Users
+
+class users(admin.ModelAdmin):
+    list_display =(
+        "name",
+        "phone_number",
+        "gender",
+        "IsPremium",
+        "IsBlocked",
+        "createdAt"
+    )
+    list_filter = ["createdAt"]
+    search_fields = ('phone_number__startswith',)
