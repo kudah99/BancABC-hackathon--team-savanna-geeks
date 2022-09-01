b322aaeb40577453acf2805cd42d20240fd812d7
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2022-09-02 00:06:54 +0200

    created user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    phone_number = models.CharField(max_length=100)
+    name = models.CharField(max_length=100 ,null=True)
+    gender = models.SmallIntegerField(null=True)
+    IsPremium = models.BooleanField(default=False)
+    IsBlocked = models.BooleanField(default=False)
+    lastSeen = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'tbl_user'
+    def __str__(self):
+        return self.phone_number
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
